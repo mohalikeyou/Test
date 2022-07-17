1e16d7db93fb2828a4344f9c1b2b271adabce68c
Pokaman <mm1314955811@163.com>
2022-07-17 14:57:25 +0800

    test

--- src/Test.java
+++ src/Test.java
@@ -8,51 +8,98 @@ import java.util.jar.JarEntry;
  */
 public class Test {
     public static void main(String[] args) {
-        new Solution().minWindow("ADOBECODEBANC"
-                ,"ABC");
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.addAtHead(1);
+        myLinkedList.addAtTail(3);
+        myLinkedList.addAtIndex(1, 2);
+        myLinkedList.get(1);
+        myLinkedList.deleteAtIndex(1);
+        myLinkedList.get(1);
     }
 }
 
-class Solution {
-    public String minWindow(String s, String t) {
-        // 窗口右元素始终添加，直到结束；
-        // 窗口左元素，如果非t则剔除窗口。或者，如果随着右元素的添加，当前左侧t元素变得冗余（即添加的右元素和当前左元素相同），
-        // 那么剔除当前冗余的左元素也不会影响滑动窗口的t元素总量！
-        // 根据这种思路，从而求得最小覆盖t窗的s串的子串；
-        if (s.length() < t.length()) return "";
+class ListNode {
+    int val;
+    ListNode next;
+    public ListNode() {}
+    public ListNode(int val) {this.val = val;}
+    public ListNode(int val, ListNode next) {this.val = val; this.next = next;}
+}
 
-        int[] hash = new int[128];
+class MyLinkedList {
+    int size;
+    ListNode head;
 
-        char[] schar = s.toCharArray();
-        char[] tchar = t.toCharArray();
+    public MyLinkedList() {
+        head = new ListNode(-1);
+        size = 0;
+    }
 
-        for (char c : tchar) ++hash[c];
-
-        String res = ""; //初始值，如果滑动窗口始终没有滑动过，则返回这个；
-
-        int nums = tchar.length; // 记录窗口内t字母的个数
-        for (int i = 0, j = 0; i < s.length(); ++i) {
-            --hash[schar[i]];//非t元素 会小于0，多余的重复t元素，也会小于零；恰好t中字母个数的都是等于零
-
-            if(hash[schar[i]] >= 0) --nums; //如果减了之后大于等于零，说明这个元素是需要的；
-
-            //如果t元素不够不会执行，一旦够了，滑动窗口会一直包含所有t元素；
-            // 关键！！ 左指针向右滑动窗口的条件？1. 左侧元素是冗余的t元素。2.左侧元素是非t内元素
-            // 删掉以上的，窗口内仍然保持有所有t元素；
-            // 冗余元素，hash值总是小于零的；
-            // 总是剔除没用元素；
-            while (nums == 0 && hash[schar[j]] < 0)
-                ++hash[schar[j++]];
-
-            // 每次 都比较目前窗口的元素的个数；如果t元素不够，不会执行
-            if (nums == 0) {
-                if (res.equals("") || res.length() > i - j + 1)
-                    res = s.substring(j, i + 1);
-            }
+    public int get(int index) {
+        if (index < 0 || index >= size) {
+            return -1;
         }
-        return "";
+        ListNode curr = head;
+        while (index >= 0) {
+            --index;
+            curr = curr.next;
+        }
+        return curr.val;
+    }
+
+    public void addAtHead(int val) {
+        ListNode curr = new ListNode(val);
+        curr.next = head.next;
+        head.next = curr;
+        ++size;
+    }
+
+    public void addAtTail(int val) {
+        ListNode curr = head;
+        while (curr.next != null) curr = curr.next;
+        curr.next = new ListNode(val);
+        ++size;
+    }
+
+    public void addAtIndex(int index, int val) {
+        if (index > size || index < 0) return;
+
+        ListNode curr = head;
+
+        while (index > 0) {
+            curr = curr.next;
+            --index;
+        }
+
+        curr.next = new ListNode(val,curr.next.next);
+        ++size;
+    }
+
+    public void deleteAtIndex(int index) {
+        if (index >= size || index < 0) return;
+
+        ListNode curr = head;
+
+        while (index > 0) {
+            curr = curr.next;
+            --index;
+        }
+
+        curr.next = curr.next.next;
+
+        --size;
     }
 }
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * MyLinkedList obj = new MyLinkedList();
+ * int param_1 = obj.get(index);
+ * obj.addAtHead(val);
+ * obj.addAtTail(val);
+ * obj.addAtIndex(index,val);
+ * obj.deleteAtIndex(index);
+ */
 
 
 
